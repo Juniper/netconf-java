da91e11f2192427258ee291a8f9b6d469115d3c0
alrighttheresham <damian.oneill@gmail.com>
2014-10-02 14:37:33 +0100

    added unit test that shows building a trap-group configuration

--- src/test/java/net/juniper/netconf/XMLBuilderTest.java
+++ src/test/java/net/juniper/netconf/XMLBuilderTest.java
@@ -17,8 +17,8 @@ public class XMLBuilderTest {
 
     @Test
     public void testCreateNewFtpConfig() throws Exception {
-        XML ftp_config = builder.createNewConfig("system", "services", "ftp");
-        assertNotNull(ftp_config);
+        XML ftpConfig = builder.createNewConfig("system", "services", "ftp");
+        assertNotNull(ftpConfig);
         String expectedOutput = "<configuration>" + NEWLINE +
                 "    <system>" + NEWLINE +
                 "        <services>" + NEWLINE +
@@ -26,7 +26,39 @@ public class XMLBuilderTest {
                 "        </services>" + NEWLINE +
                 "    </system>" + NEWLINE +
                 "</configuration>";
-        assertEquals(expectedOutput, ftp_config.toString().trim());
+        assertEquals(expectedOutput, ftpConfig.toString().trim());
+    }
+
+
+    @Test
+    public void testCreateNewTrapGroupConfig() throws Exception {
+        XML trapGroupConfig = builder.createNewConfig("snmp");
+        XML trapGroup = trapGroupConfig.addPath("trap-group");
+        trapGroup.append("group-name", "new-trap-receiver");
+        XML categories = trapGroup.append("categories");
+        categories.append("chassis");
+        categories.append("link");
+        trapGroup.append("destination-port", "162");
+        XML targets = trapGroup.append("targets");
+        targets.append("name", "10.0.0.1");
+
+
+        String expectedOutput = "<configuration>" + NEWLINE +
+                "    <snmp>" + NEWLINE +
+                "        <trap-group>" + NEWLINE +
+                "            <group-name>new-trap-receiver</group-name>" + NEWLINE +
+                "            <categories>" + NEWLINE +
+                "                <chassis/>" + NEWLINE +
+                "                <link/>" + NEWLINE +
+                "            </categories>" + NEWLINE +
+                "            <destination-port>162</destination-port>" + NEWLINE +
+                "            <targets>" + NEWLINE +
+                "                <name>10.0.0.1</name>" + NEWLINE +
+                "            </targets>" + NEWLINE +
+                "        </trap-group>" + NEWLINE +
+                "    </snmp>" + NEWLINE +
+                "</configuration>";
+        assertEquals(expectedOutput, trapGroupConfig.toString().trim());
     }
 
 }
